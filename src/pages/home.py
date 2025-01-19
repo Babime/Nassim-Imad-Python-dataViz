c5336f8c65db85fc2a2a136488c8cefde9b76172
Ashlad <imadbenbp@gmail.com>
2025-01-19 16:32:16 +0100

    Updated home page

--- src/pages/home.py
+++ src/pages/home.py
@@ -11,7 +11,10 @@ layout = html.Div(
         html.H1("Bienvenue sur le Dashboard League of Legends", className="h1"),
         # paragraphe descriptif
         html.P(
-            "Explorez les statistiques et analyses des champions, objectifs, et performances.",
+            "Explorez les statistiques et analyses des champions, objectifs, et performances. "
+            "Ce tableau de bord interactif vous permet de visualiser les données des matchs de League of Legends, "
+            "d'analyser les performances des joueurs, de suivre l'évolution des golds, des dégâts infligés, et bien plus encore. "
+            "Utilisez les différents graphiques et filtres pour obtenir des insights détaillés et améliorer votre compréhension du jeu.",
             style={"color": "#eaeaea", "textAlign": "center"},
         ),
     ],
